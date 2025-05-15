32cbdbb6f17426f39b6764ab66467f45bb026246
張家瑋 <163541105+deucalion1080@users.noreply.github.com>
2025-05-15 13:47:34 +0000

    package_module

---
+++ lesson6/edu_test/__init__.py
@@ -0,0 +1,1 @@
+from . import les06_tools
--- lesson6/lesson6_1.py
+++ lesson6/lesson6_1.py
@@ -1,13 +1,14 @@
 #BMI 偵測體質版本 + elif 
 #ipynb =>測試與學習用
 #import les06_tools
-from les06_tools import caculate_bmi,get_state
+#from les06_tools import caculate_bmi,get_state
+import edu_test
 
 def main():
     try:
         height:float = float(input("請輸入您的身高(要120~220公分之間):"))        
         weight:float = float(input("請輸入您的體重(要30~200公斤之間):"))        
-        BMI = caculate_bmi(height,weight)
+        BMI = edu_test.les06_tools.caculate_bmi(height,weight)
     
     except ValueError:       
             print("輸入值錯誤!請重新輸入!")    
@@ -17,7 +18,7 @@ def main():
         print(f"您的身高是:{height}公分")
         print(f"您的體重是:{weight}公斤")
         print(f"您的BMI結果是:{BMI:.2f}")
-        print(get_state(BMI))
+        print(edu_test.les06_tools.get_state(BMI))
         
     print("BMI偵測結束!") 
 if __name__ == "__main__":
